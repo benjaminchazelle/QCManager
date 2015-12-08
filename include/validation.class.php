74e7d6911f3ea70dcf8244576fae74a3d11e37ce
Valentin <valentin.bourdier@mail.com>
2015-12-08 09:47:23 +0100

    last update : added datetime type

--- include/validation.class.php
+++ include/validation.class.php
@@ -14,6 +14,7 @@ class Validation {
 		Validation::$f->{"Int"} = function ($d) { return is_numeric($d) && ($d%1 == 0); };	
 		Validation::$f->{"Number"} = function ($d) { return is_numeric($d); };	
 		Validation::$f->{"notEmpty_String"} = function ($d) { return strlen($d) > 0; };	
+		Validation::$f->{"datetime"} = function ($d) { DateTime::createFromFormat('Y-m-d H:i:s', '2009-02-15 15:16:17') != false; };	
 		Validation::$f->{"Email"} = function ($d) { return filter_var($d, FILTER_VALIDATE_EMAIL); };	
 		Validation::$f->{"Timestamp"} = Validation::$f->Int;	
 		Validation::$f->{"anterior_Timestamp"} = function ($d) { return $d < time(); };	
