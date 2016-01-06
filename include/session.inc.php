991ec73127b061f3d07dae1587a0e605609a6bc1
benjaminchazelle <bchazelle.dev@outlook.fr>
2016-01-06 16:30:39 +0100

    qcmanager production host setting modified / www subdomain forced

--- include/session.inc.php
+++ include/session.inc.php
@@ -1,6 +1,11 @@
 <?php
 
-session_start();
+if($_SERVER["SERVER_NAME"] == "qcmanager.tk") {
+	header('Status: 301 Moved Permanently', false, 301);   
+	header("Location: http://www.qcmanager.tk".$_SERVER["REQUEST_URI"]);
+	exit;
+}
 
+session_start();
 
 ?>
